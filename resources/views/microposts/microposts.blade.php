53c8ef0955c9468eb67f802a44a8a0668a5357ac
Ubuntu <ubuntu@ip-172-31-24-154.ap-northeast-1.compute.internal>
2021-05-19 23:53:01 +0000

    modify favorite button

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -15,16 +15,16 @@
                         {{-- 投稿内容 --}}
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
                     </div>
-                    <div>
-                        @if (Auth::id() == $micropost->user_id)
-                            {{-- 投稿削除ボタンのフォーム --}}
-                            {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
-                                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
-                            {!! Form::close() !!}
-                        @endif
-                    </div>
                         {{-- お気に入り登録／解除ボタン --}}
                         @include('user_favorite.favorite_button')
+                        <div>
+                            @if (Auth::id() == $micropost->user_id)
+                                {{-- 投稿削除ボタンのフォーム --}}
+                                {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                                    {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm col-md-2']) !!}
+                                {!! Form::close() !!}
+                            @endif
+                       </div>
                 </div>
             </li>
         @endforeach
--- resources/views/user_favorite/favorite_button.blade.php
+++ resources/views/user_favorite/favorite_button.blade.php
@@ -2,11 +2,11 @@
 @if (Auth::user()->is_favoring($micropost->id))
     {{-- お気に入り解除ボタンのフォーム --}}
     {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-        {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
+        {!! Form::submit('Unfavorite', ['class' => "btn btn-success btn-block col-md-2"]) !!}
     {!! Form::close() !!}
 @else
     {{-- お気に入り登録ボタンのフォーム --}}
     {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-        {!! Form::submit('Favorite', ['class' => "btn btn-primary btn-block"]) !!}
+        {!! Form::submit('Favorite', ['class' => "btn btn-light btn-block col-md-2"]) !!}
     {!! Form::close() !!}
 @endif
